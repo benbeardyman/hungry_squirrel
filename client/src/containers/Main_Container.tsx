946f974394fe22526adae447aae74b99af0b407f
Ben Barlow <ben.barlow@yahoo.co.uk>
2023-05-28 16:01:15 +0100

    added main logo

--- client/src/containers/Main_Container.tsx
+++ client/src/containers/Main_Container.tsx
@@ -1,8 +1,13 @@
+import logo from "../img/hs_logo_2_text.png"
+
 const MainContainer = () => {
     return (
-        <header className="App-header">
-            Hello Hungry Squirrel! What can we get you for dinner?
-        </header>
+        <>
+            <header className="App-header">
+                <img src={logo} />
+                Hello Hungry Squirrel! What can we get you for dinner?
+            </header>
+        </>
     )
 }
 
