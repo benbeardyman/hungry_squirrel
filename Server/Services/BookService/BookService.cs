fdfe332d97fd1716f79463d17f9e15f0bf55f224
Ben Barlow <ben.barlow@yahoo.co.uk>
2023-06-26 10:07:30 +0100

    ef migrations

--- Server/Services/BookService/BookService.cs
+++ Server/Services/BookService/BookService.cs
@@ -13,9 +13,11 @@ namespace server.Services.BookService
             new Book { Id = 1, Title= "Vegan Pizza" }
         };
         private readonly IMapper mapper;
+        private readonly DataContext context;
 
-        public BookService(IMapper mapper)
+        public BookService(IMapper mapper, DataContext context)
         {
+            this.context = context;
             this.mapper = mapper;
         }
 
@@ -23,24 +25,28 @@ namespace server.Services.BookService
         {
             var serviceResponse = new ServiceResponse<List<GetBookDto>>();
             var book = mapper.Map<Book>(newBook);
-            book.Id = books.Max(book => book.Id) + 1;
-            books.Add(book);
-            serviceResponse.Data = books.Select(book => mapper.Map<GetBookDto>(book)).ToList();
+
+            context.Books.Add(book);
+            await context.SaveChangesAsync();
+
+            serviceResponse.Data = 
+                await context.Books.Select(book => mapper.Map<GetBookDto>(book)).ToListAsync();
             return serviceResponse;
         }
 
         public async Task<ServiceResponse<List<GetBookDto>>> GetAllBooks()
         {
             var serviceResponse = new ServiceResponse<List<GetBookDto>>();
-            serviceResponse.Data = books.Select(book => mapper.Map<GetBookDto>(book)).ToList();
+            var dbBooks = await context.Books.ToListAsync();
+            serviceResponse.Data = dbBooks.Select(book => mapper.Map<GetBookDto>(book)).ToList();
             return serviceResponse;
         }
 
         public async Task<ServiceResponse<GetBookDto>> GetBookById(int id)
         {
             var serviceResponse = new ServiceResponse<GetBookDto>();
-            var book = books.FirstOrDefault(book => book.Id == id);
-            serviceResponse.Data = mapper.Map<GetBookDto>(book);
+            var dbBook = await context.Books.FirstOrDefaultAsync(book => book.Id == id);
+            serviceResponse.Data = mapper.Map<GetBookDto>(dbBook);
             return serviceResponse;
         }
     }
