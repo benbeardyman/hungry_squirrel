822257c0356d0fc9a7b32ed840975964008e5b79
Ben Barlow <ben.barlow@yahoo.co.uk>
2023-08-13 21:41:22 +0100

    update

--- client/src/app/picker/page.tsx
+++ client/src/app/picker/page.tsx
@@ -2,11 +2,15 @@ import Link from 'next/link';
 
 export default function Picker() {
   return (
-    <header className='flex gap-2'>
-      <h1>Picker</h1>
-      <Link href='/' className='border px-2 py-1 rounded'>
-        Home
-      </Link>
-    </header>
+    <>
+      <header className='flex justify-center text-5xl'>
+        <h1>Picker</h1>
+      </header>
+      <div className='flex justify-center'>
+        <Link href='/' className='border px-2 py-1 rounded'>
+          Home
+        </Link>
+      </div>
+    </>
   );
 }
