f8e585d9666bc6614b8e7908e8ce33d92f458f4e
Ben Barlow <ben.barlow@yahoo.co.uk>
2023-08-15 20:44:24 +0100

    updated form

--- client/src/app/books/add_book/page.tsx
+++ client/src/app/books/add_book/page.tsx
@@ -1,6 +1,18 @@
+'use client';
+
 import Link from 'next/link';
+import { useState } from 'react';
 
 export default function AddBook() {
+  const [val, setVal] = useState('');
+
+  const handlePagesChange = (e) => {
+    const inputValue = e.target.value;
+    if (/^\d{0,4}$/.test(inputValue)) {
+      setVal(inputValue);
+    }
+  };
+
   return (
     <>
       <div className='flex justify-center gap-2 mt-4'>
@@ -28,16 +40,42 @@ export default function AddBook() {
             placeholder='Title'
             className='border-2 border-slate-800 dark:border-slate-100 bg-transparent px-2 py-1 rounded outline-none focus-within:border-slate-200'
           />
+          <input
+            type='text'
+            name='description'
+            placeholder='Description'
+            className='border-2 border-slate-800 dark:border-slate-100 bg-transparent px-2 py-1 rounded outline-none focus-within:border-slate-200'
+          />
+          <input
+            type='text'
+            value={val}
+            onChange={handlePagesChange}
+            name='pages'
+            placeholder='Pages'
+            className='border-2 border-slate-800 dark:border-slate-100 bg-transparent px-2 py-1 rounded outline-none focus-within:border-slate-200'
+          />
+          <input
+            type='text'
+            name='image'
+            placeholder='Image URL'
+            className='border-2 border-slate-800 dark:border-slate-100 bg-transparent px-2 py-1 rounded outline-none focus-within:border-slate-200'
+          />
+          <input
+            type='text'
+            name='url'
+            placeholder='URL'
+            className='border-2 border-slate-800 dark:border-slate-100 bg-transparent px-2 py-1 rounded outline-none focus-within:border-slate-200'
+          />
           <div className='flex gap-1 justify-end'>
             <Link
               href='/books'
-              className='border-2 border-slate-800 dark:border-slate-100 text-slate-300 px-2 py-1 rounded hover:bg-slate-50 dark:hover:bg-slate-600 focus-within:bg-slate-50  dark:focus-within:bg-slate-600 outline-none'
+              className='border-2 border-slate-800 dark:border-slate-100 px-2 py-1 rounded hover:bg-slate-50 dark:hover:bg-slate-600 focus-within:bg-slate-50  dark:focus-within:bg-slate-600 outline-none'
             >
               Cancel
             </Link>
             <button
               type='submit'
-              className='border-2 border-slate-800 dark:border-slate-100 text-slate-300 px-2 py-1 rounded hover:bg-slate-50 dark:hover:bg-slate-600 focus-within:bg-slate-50  dark:focus-within:bg-slate-600 outline-none'
+              className='border-2 border-slate-800 dark:border-slate-100  px-2 py-1 rounded hover:bg-slate-50 dark:hover:bg-slate-600 focus-within:bg-slate-50  dark:focus-within:bg-slate-600 outline-none'
             >
               Add
             </button>
