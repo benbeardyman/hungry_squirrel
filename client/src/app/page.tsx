6697e97fbf3ba0309932abb93711b8eef02061bb
Ben Barlow <ben.barlow@yahoo.co.uk>
2023-08-15 15:50:17 +0100

    updated logo

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <header className='flex gap-1 flex-col items-center justify-center h-screen'>
       <ThemeImage
-        srcLight='/images/hs_logo_light.png'
-        srcDark='/images/hs_logo_dark.png'
+        srcLight='/images/hs_logo_lt.png'
+        srcDark='/images/hs_logo_dk.png'
         width='930'
         height='300'
         alt='app logo'
